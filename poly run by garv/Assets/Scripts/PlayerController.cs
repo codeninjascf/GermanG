73d569c93434d8bc0fefab787d6a8aed0b77c714
codeninjascf <cnchandlersford@gmail.com>
2023-09-30 13:26:13 +0100

    REY

---
+++ poly run by garv/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 25f;
+
+     private bool _isGrounded;
+     private Rigidbody2D _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && _isGrounded)
+        {
+            _rigidbody.velocity = Vector2.zero;
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void onCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            if (PlayerCollisions.CollidedWithSide(gameObject, other.gameObject, PlayerCollisions.Side.Bottom))
+            {
+                _isGrounded = true;
+            }
+            else
+            {
+                GameManager.EndGame();
+            }
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            GameManager.EndGame();
+        }
+    }
+
+    void onCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    void onCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+        }
+    }
+
+    void OnTiggerEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            GameManager.Score += 25;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
+
