0687410223c2204dbe683d7c02f824202bd31184
George Chadwick <84614341+chadderz-css@users.noreply.github.com>
2024-04-28 12:56:19 +0100

    Evil Fort finished
    tgceujgduwgu[hqwhfw3udh[IH  23 YW HIJHIY

--- Evil Fortress/Assets/Scripts/DoorSwitch.cs
+++ Evil Fortress/Assets/Scripts/DoorSwitch.cs
@@ -6,12 +6,20 @@ public class DoorSwitch : MonoBehaviour
 {
     public GameObject enabledObject;
     public GameObject disabledObject;
+
+    public AudioClip enabledSound;
+    public AudioClip disabledSound;
+
+    private AudioSource _audioSource;
+
     void Start()
     {
       enabledObject.SetActive(false);
       disabledObject.SetActive(true);
 
       SwitchEnabled = false;
+
+      _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,6 +48,10 @@ public class DoorSwitch : MonoBehaviour
       disabledObject.SetActive(false);
 
       SwitchEnabled = true;
+
+      _audioSource.clip = enabledSound;
+      _audioSource.Play();
+
     }
     void OnTriggerStay(Collider other)
     {
@@ -47,5 +59,8 @@ public class DoorSwitch : MonoBehaviour
       disabledObject.SetActive(false);
 
       SwitchEnabled = true;
+
+      _audioSource.clip = disabledSound;
+      _audioSource.Play();
     }
 }
--- Evil Fortress/Assets/Scripts/DoorTrigger.cs
+++ Evil Fortress/Assets/Scripts/DoorTrigger.cs
@@ -8,6 +8,8 @@ public class DoorTrigger : MonoBehaviour
 
     private bool _opened;
     private Animator _animator;
+
+    private AudioSource _audioSource;
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -29,8 +31,10 @@ public class DoorTrigger : MonoBehaviour
                 {
                     _animator.SetBool("DoorActivate", true);
                     _opened = true;
-                }
+
+                    _audioSource.Play();
             }
         }
+    }
     }
 }
