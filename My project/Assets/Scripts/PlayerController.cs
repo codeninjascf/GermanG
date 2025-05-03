1ec23b3753f8641c6174f47df020c66bee05b07e
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-05-03 12:15:28 +0100

    camera metrroooooo
    if yung metro dont trust you im gon shoot you...

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
 
    public float moveSpeed;
    public float jumpForce;
+   public Transform groundPoint;
+   private bool isOnGround;
+   public LayerMask whatIsGround;
+
+   public Animator anim;
 
     void Start()
     {
@@ -21,11 +26,24 @@ public class PlayerController : MonoBehaviour
         {
             theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
 
-            if(Input.GetButtonDown("Jump"))
+            if (theRB.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            } else if(theRB.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
+
+            isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
+
+            if(Input.GetButtonDown("Jump") && isOnGround)
             {
                 theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
             }
         }
+
+        anim.SetBool("isOnGround", isOnGround);
+        anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
 
     }
 }
