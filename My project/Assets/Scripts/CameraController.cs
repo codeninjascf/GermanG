5dbf5376053e4091c439bbb3ce12a6d337592423
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-05-17 12:03:52 +0100

    metrooooooooo Bullett ting
    metro metro metro

--- My project/Assets/Scripts/CameraController.cs
+++ My project/Assets/Scripts/CameraController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private PlayerController player;
+    public BoxCollider2D boundsBox;
+
+    private float halfHeight, halfWidth;
 
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect; 
     }
 
     // Update is called once per frame
@@ -16,7 +22,11 @@ public class CameraController : MonoBehaviour
     {
         if(player != null)
         {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+            transform.position = new Vector3(
+                Mathf.Clamp(player.transform.position.x, boundsBox.bounds.min.x + halfWidth, boundsBox.bounds.max.x - halfWidth),
+                Mathf.Clamp(player.transform.position.y, boundsBox.bounds.min.y + halfHeight, boundsBox.bounds.max.y - halfHeight),
+                transform.position.z);
+
         }
     }
 }
